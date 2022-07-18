0afa872fc1de8166ab810a54cc490825515ebe79
CaptainDario <51273483+CaptainDario@users.noreply.github.com>
2022-07-18 22:32:42 +0200

    removed unrelated stuff

--- test/kagome_dart_test.dart
+++ test/kagome_dart_test.dart
@@ -3,10 +3,4 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:kagome_dart/kagome_dart.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
 }
