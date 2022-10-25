48d3d833b6863ecda6d09200ac2d602dd3d67ee6
dario windows desktop <daapplab@gmail.com>
2022-10-25 19:45:09 +0200

    use debug print

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:kagome_dart/kagome_dart.dart';
 
 import 'package:tuple/tuple.dart';
@@ -13,21 +14,21 @@ void main() {
   // important to call before any other methods
   kagome.initTokenizer();
 
-  // create space in input
+  // create spaces in input
   List w = kagome.runWakati(input);
   for (var i = 0; i < w.length; i++) {
-    print(w[i]);
+    debugPrint(w[i]);
   }
 
   // analyze and create spaces
   Tuple2<List<String>, List<List<String>>> t = 
     kagome.runAnalyzer(input, AnalyzeModes.search);
   for (var i = 0; i < t.item1.length; i++) {
-    print("${t.item1[i]} \t ${t.item2[i]}");
+    debugPrint("${t.item1[i]} \t ${t.item2[i]}");
   }
   
   t = kagome.runAnalyzer(input, AnalyzeModes.normal);
-  print(t);
+  debugPrint(t.toString());
 
   kagome.destroyTokenizer();
 }
